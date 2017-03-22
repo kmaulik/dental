f2b34e14e2225ae61205951b63dc35ca2bd18968
dhknarola <dhk@narola.email>
2017-03-22 10:55:03 +0530

    Chnages

--- application/controllers/Rfp.php
+++ application/controllers/Rfp.php
@@ -288,7 +288,7 @@ class Rfp extends CI_Controller {
 				if($res){
 					$this->session->set_flashdata('success', 'Step 3 of 3  completed -Financial Information Updated Successfully - Kindly Review your Information and submit your Treatment Plan');
 					if($this->input->post('prev')){
-						redirect('rfp/edit/'.encode(encode($this->session->userdata['rfp_data']['rfp_last_id'])).'/1'); // Go to 2nd step (Treatment Plan Details)
+						redirect('rfp/edit/'.encode($this->session->userdata['rfp_data']['rfp_last_id']).'/1'); // Go to 2nd step (Treatment Plan Details)
 					}else{
 						redirect('rfp/add/3'); // Goto 4th step (Summary Page - Final)
 					}	
